08377e879316ca4f679089083f6ecc067246a2cc
Rajat Bansal <rjtbansal@gmail.com>
2020-12-07 21:40:22 -0500

    able to load characters via the api

---
+++ src/components/CharacterDropdown/index.js
@@ -0,0 +1,34 @@
+import { FormControl, InputLabel, MenuItem, Select } from "@material-ui/core";
+import { useState } from "react";
+import { makeStyles } from '@material-ui/core/styles';
+
+const useStyles = makeStyles((theme) => ({
+  formControl: {
+    margin: theme.spacing(1),
+    minWidth: 120,
+  }
+}));
+
+const CharacterDropdown = ({ characterProps }) => {
+   const classes = useStyles();
+  const [character, setCharacter] = useState('');
+
+  const handleCharacterSelection = event => {
+    setCharacter(event.target.value);
+  }
+
+  return (
+    <FormControl className={classes.formControl}>
+      <InputLabel id="character-label">
+        Select Character 
+      </InputLabel>
+      <Select labelId="character-label" id="character-select" value={character} onChange={handleCharacterSelection}>
+        {
+          characterProps.map(character => <MenuItem value={character.name.toLowerCase()}>{ character.name }</MenuItem>)
+        }
+      </Select>
+    </FormControl>
+  )
+}
+
+export default CharacterDropdown;
--- src/containers/StarWarsCharacters/index.js
+++ src/containers/StarWarsCharacters/index.js
@@ -1,9 +1,42 @@
-import { Typography } from "@material-ui/core";
+import { Container, LinearProgress, Typography } from "@material-ui/core";
+import { makeStyles } from "@material-ui/styles";
+import CharacterDropdown from "../../components/CharacterDropdown";
+import axios from "axios";
+import { useEffect, useState } from "react";
+
+const useStyles = makeStyles((theme) => ({
+  starWarsTitle: {
+    textAlign: "center",
+    marginTop: "2rem"
+  }
+}));
 
 const StarWarsCharacters = () => {
-  return <Typography variant="h1" body="h1">
-    Welcome to StarWars Characters!
-  </Typography>
+
+  const classes = useStyles();
+  const [charactersFetched, setCharactersFetched] = useState([]);
+  const loadCharacters = async() => {
+    try{
+      const response = await axios.get("https://cors-anywhere.herokuapp.com/https://swapi.dev/api/people");
+      setCharactersFetched(response.data.results);
+    } catch (err) {
+      console.error(err);
+    }
+  }
+
+  useEffect(() => {
+    loadCharacters();
+  }, []);
+
+  return charactersFetched.length ? (
+    <Container>
+      <Typography variant="h1" body="h1" className={classes.starWarsTitle}>
+        Welcome to StarWars Characters!
+     </Typography>
+      <CharacterDropdown characterProps={charactersFetched} />
+    </Container>
+  ) :
+    <LinearProgress />
 }
 
 export default StarWarsCharacters;
